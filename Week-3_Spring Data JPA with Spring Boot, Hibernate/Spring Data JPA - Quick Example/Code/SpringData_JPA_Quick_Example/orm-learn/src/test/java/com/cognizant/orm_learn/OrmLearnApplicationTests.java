b3deaf9d45eda7510da072acf89b3b2bc29e1b5e
omonikpaparao <omonikpaparao@gmail.com>
2025-07-04 07:06:28 +0530

    Week-3

--- Week-3_Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA - Quick Example/Code/SpringData_JPA_Quick_Example/orm-learn/src/test/java/com/cognizant/orm_learn/OrmLearnApplicationTests.java
+++ Week-3_Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA - Quick Example/Code/SpringData_JPA_Quick_Example/orm-learn/src/test/java/com/cognizant/orm_learn/OrmLearnApplicationTests.java
@@ -2,7 +2,7 @@ package com.cognizant.orm_learn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//import completed
 @SpringBootTest
 class OrmLearnApplicationTests {
 
